1ea692dd954c62c948dd1b9b56f16a5f92176015
NJAbuin <53914955+NJAbuin@users.noreply.github.com>
2019-10-28 12:39:32 -0300

    1

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+import Main from "./views/Main";
+
+ReactDOM.render(<Main />, document.getElementById("root")); //ADD NEW COMPONENTS TO MAIN
---
+++ src/views/Main.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from "../components/Header";
+import axios from "axios";
+
+export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return <div>HOMEPAGE</div>;
+  }
+}
